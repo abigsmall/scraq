93272ae8b182686d95edd34a12683f01402360fd
abigsmall <abigsmall@gmail.com>
2014-02-09 16:27:45 -0500

    my first commit to this cool quotes scraper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scrak::Application.config.secret_key_base = '9219c0cac0971939b50ce63a19f55bd0574e16395ccb9bd6f32102bbe4785f0fe8c3baba65529224a0dd5cf6160022c76eddd0cbed32b918ff6df34cbcf76776'
